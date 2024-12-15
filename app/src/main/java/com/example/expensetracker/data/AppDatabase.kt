a40dbd8937362ae252916a6c164cd4fd3763f371
MOVING <404559619@qq.com>
2024-12-15 17:05:41 +0800

    12/15 接入Room 数据库DB 2， 需要改的代码比较多，需要整体review

--- app/src/main/java/com/example/expensetracker/data/AppDatabase.kt
+++ app/src/main/java/com/example/expensetracker/data/AppDatabase.kt
@@ -17,20 +17,20 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun expenseDao(): ExpenseDao
     abstract fun categoryDao(): CategoryDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "expense_tracker_db"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: AppDatabase? = null
+//
+//        fun getDatabase(context: Context): AppDatabase {
+//            return INSTANCE ?: synchronized(this) {
+//                val instance = Room.databaseBuilder(
+//                    context.applicationContext,
+//                    AppDatabase::class.java,
+//                    "expense_tracker_db"
+//                ).build()
+//                INSTANCE = instance
+//                instance
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/example/expensetracker/di/RepositoryModule.kt
+++ app/src/main/java/com/example/expensetracker/di/RepositoryModule.kt
@@ -1,8 +1,11 @@
 package com.example.expensetracker.di
+import android.content.Context
+import androidx.room.Room
 import com.example.expensetracker.data.*
 import com.example.expensetracker.repository.*
 import dagger.*
 import dagger.hilt.*
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.*
 import javax.inject.*
 
@@ -20,5 +23,15 @@ object RepositoryModule {
     @Singleton
     fun provideCategoryRepository(): CategoryRepository {
         return MockCategoryRepository()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context):AppDatabase{
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "expense_tracker_db"
+        ).build()
     }
 }
