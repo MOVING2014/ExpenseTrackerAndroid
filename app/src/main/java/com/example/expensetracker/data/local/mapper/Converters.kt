b016221d96e694433f19c8ff120c8a748597b1ac
MOVING <404559619@qq.com>
2024-12-19 08:32:11 +0800

    12/19 增加了部分convertor逻辑，放在注释里了

--- app/src/main/java/com/example/expensetracker/data/local/mapper/Converters.kt
+++ app/src/main/java/com/example/expensetracker/data/local/mapper/Converters.kt
@@ -1,7 +1,10 @@
 package com.example.expensetracker.data.local.mapper
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.room.TypeConverter
 import java.sql.Date
+import kotlin.reflect.KClass
 
 // data/converter/Converters.kt
 class Converters {
@@ -24,4 +27,22 @@ class Converters {
     fun toString(list: List<String>): String {
         return list.joinToString(",")
     }
+//    // 将字符串转换为对应的 ImageVector
+//    @TypeConverter
+//    fun fromStringToImageVector(iconName: String): ImageVector? {
+//        return stringToImageVector(iconName)  // 使用反射转换函数
+//    }
+//
+//    // 将 ImageVector 转换为字符串
+//    @TypeConverter
+//    fun fromImageVectorToString(icon: ImageVector): String {
+//        return iconToString(icon)  // 将 ImageVector 转换为字符串
+//    }
+//
+//
+//    // 将字符串转化为对应的 ImageVector
+//    private fun stringToImageVector(iconName: String): ImageVector? {
+//        // 解析字符串
+//    }
+
 }
